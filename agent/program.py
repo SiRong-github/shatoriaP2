8e75bf70bc336d376eac23507f20ddad5e21eadf
vockoi <victoria.halim3@gmail.com>
2023-05-10 15:43:34 +1000

    change name

--- agent/program.py
+++ agent/program.py
@@ -2,13 +2,16 @@
 # Project Part B: Game Playing Agent
 
 from referee.game import \
-    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
+    PlayerColor, Action, SpawnAction, SpreadAction, HexPos
 
 from helperFunctions.action_helpers import *
 from helperFunctions.boardHelpers import *
 from helperFunctions.tupleOperators import *
 from helperFunctions.utils import *
-import random
+from .miniMaxTreeHelpers import *
+
+#logging.basicConfig(level=logging.WARNING, filename="logfile.txt", filemode="a+",
+    #format="%(asctime)-15s %(levelname)-8s %(message)s")
 
 # This is the entry point for your game playing agent. Currently the agent
 # simply spawns a token at the centre of the board if playing as RED, and
@@ -16,8 +19,8 @@ import random
 # intended to serve as an example of how to use the referee API -- obviously
 # this is not a valid strategy for actually playing the game!
 
-board = dict()
-own = dict()
+board = dict() # (x, y) : (colour, power)
+own = dict() # (x, y) : power
 opponent = dict()
 currTotalPower = 0
 
@@ -26,6 +29,7 @@ class Agent:
         """
         Initialise the agent.
         """
+        
         self._color = color
         match color:
             case PlayerColor.RED:
@@ -38,69 +42,50 @@ class Agent:
         Return the next action to take.
         """
         global currTotalPower
-        randomCells = []
+        global turn
+        global turns_left
+
+        if (len(board) == 0):
+            turn = 1
+            turns_left = 171
+        elif (len(board) == 1):
+            turn = 0
+            turns_left = 171
 
         # Determine if opening move
-        if (len(board) < 2):
-            x = random.randint(0,6)
-            y = random.randint(0,6)
-            match self._color:
-                case PlayerColor.RED:
-                    return SpawnAction(HexPos(x, y))
-                case PlayerColor.BLUE:
-                    while(not isValidSpawnMove(board, x, y)):
-                        while(randomCells and (x,y) in randomCells):
-                            x = random.randint(0,6)
-                            y = random.randint(0,6)
-                    cell = (x,y)
-                    randomCells.append(cell)
-                    return SpawnAction(HexPos(x,y))
+        if (len(board) < 2) and self._color == PlayerColor.RED:
+            #logging.critical(f"{self._color}, TURN: {turn}")
+            return SpawnAction(HexPos(3, 3)) # easiest to visualise
+        
         else:
-            randoAction = random.randint(0,1)
-            
-            # Randomly select action type
-            if randoAction == 0 and currTotalPower <= MAX_TOTAL_POWER:
-                x = random.randint(0,6)
-                y = random.randint(0,6)
-                while(not isValidSpawnMove(board, x, y)):
-                    while(randomCells and (x,y) in randomCells):
-                        x = random.randint(0,6)
-                        y = random.randint(0,6)
-                    cell = (x,y)
-                    randomCells.append(cell)
-                return SpawnAction(HexPos(x,y))
-            else: 
-                ownCopy = dict(own)
-                
-                # Check if can spread
-                while(ownCopy):
-                    randoIndex = random.randint(0,len(ownCopy)-1)
-                    randomDirection = random.randint(0,5)
-                    count = 0
-                    for k in ownCopy.keys():
-                        if count == randoIndex:
-                            x = k[0]
-                            y = k[1]
-                            direction = getRandoDir(randomDirection)
-                            return SpreadAction(HexPos(x,y), HexDir(direction))           
-                        count += 1
-                    ownCopy.pop(k)
+            turn += 2
+            #logging.critical(f"{self._color}, TURN: {turn}")
+            move = miniMaxTree(board, self._color, turns_left, referee["time_remaining"])
+            cell = HexPos(move[0][0], move[0][1])
+            turns_left -= 1
+
+            if move[1] == (0, 0):
+                return SpawnAction(cell)
+            else:
+                dir = tupleDirectionConverter(move[1])
+                return SpreadAction(cell, dir)
 
     def turn(self, color: PlayerColor, action: Action, **referee: dict):
         """
         Update the agent with the last player's action.
         """
         global currTotalPower
+
         match action:
             case SpawnAction(cell):
-                print(f"Testing: {color} SPAWN at {cell}")
+                #print(f"Testing: {color} SPAWN at {cell}")
                 if color == self._color:
                     currTotalPower = updateBoardSpawn(tuple(cell), color, board, own, currTotalPower)
                 else:
                     currTotalPower = updateBoardSpawn(tuple(cell), color, board, opponent, currTotalPower)
                 pass
             case SpreadAction(cell, direction):
-                print(f"Testing: {color} SPREAD from {cell}, {direction}")
+                #print(f"Testing: {color} SPREAD from {cell}, {direction}")
                 if color == self._color:
                     currTotalPower = updateBoardSpread(tuple(cell), directionTupleConverter(direction), color, board, own, opponent, currTotalPower)
                 else:
@@ -108,10 +93,19 @@ class Agent:
                 pass
 
 # Testing
-# agent first: python3 -m referee agent greedySearchAgent:GreedyAgent
-# GreedyAgent first: python3 -m referee greedySearchAgent:GreedyAgent agent
-# referee["time_remaining"]
-# referee["space_remaining"]
-# referee["space_limit"]
+
+# python -m referee -t 90 miniMaxBFSAgent:MiniMaxBFSAgent miniMaxAlphaBeta:MiniMaxAlphaBeta
+
+# python -m referee -t 180 -s 250 miniMaxAlphaBeta:MiniMaxAlphaBeta miniMaxBFSAgent:MiniMaxBFSAgent
+# python -m referee -t 180 -s 250 miniMaxBFSAgent:MiniMaxBFSAgent miniMaxAlphaBeta:MiniMaxAlphaBeta
+# python -m referee -t 180 -s 250 miniMaxAlphaBeta:MiniMaxAlphaBeta greedySearchAgent:GreedyAgent
+# python -m referee -t 180 miniMaxAlphaBeta:MiniMaxAlphaBeta miniMaxAlphaBeta:MiniMaxAlphaBeta
+
+# python -m referee -t 180 miniMaxBFSAgent:MiniMaxBFSAgent greedySearchAgent:GreedyAgent
+# python -m referee -t 180 miniMaxAlphaBeta:MiniMaxAlphaBeta greedySearchAgent:GreedyAgent
+# python -m referee -t 180 greedySearchAgent:GreedyAgent miniMaxAlphaBeta:MiniMaxAlphaBeta 
+# python -m referee -t 180 greedySearchAgent:GreedyAgent miniMaxBFSAgent:MiniMaxBFSAgent 
+
 # Help
 # python3 -m referee -h
+
